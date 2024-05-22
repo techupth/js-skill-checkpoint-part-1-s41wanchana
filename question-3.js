f4c7d3b52b8a18b32ad31431c32a970c3704445d
Wanchana <s.wanchana41@gmail.com>
2024-05-22 13:56:37 +0700

    feat: add checkPasswordStrength function, q.3

--- question-3.js
+++ question-3.js
@@ -1,3 +1,19 @@
 // Question #3
-let userPassword = "";
+let userPassword1 = "swnalWadqQ";
+let userPassword2 = "TechUp";
+let userPassword3 = "abcde";
 // เริ่มเขียนโค้ดตรงนี้
+
+function checkPasswordStrength(password) {
+  if (password.length < 6) {
+    return "Weak";
+  } else if (password.length >= 6 && password.length < 10) {
+    return "Medium";
+  } else {
+    return "Strong";
+  }
+}
+
+console.log(checkPasswordStrength(userPassword1));
+console.log(checkPasswordStrength(userPassword2));
+console.log(checkPasswordStrength(userPassword3));
