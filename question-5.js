6d28eb30a28622945cd9bc98894a8eceae2447c8
Wanchana <s.wanchana41@gmail.com>
2024-05-22 14:36:11 +0700

    feat: add calculateTotalPrice with promotionCode, q.5

--- question-5.js
+++ question-5.js
@@ -17,5 +17,26 @@ const products = [
   },
 ];
 
-const promotionCode = "";
+const promotionCode = "SALE50";
 // เริ่มเขียนโค้ดตรงนี้
+
+function calculateTotalPrice(products, promotionCode) {
+  let value = [];
+  for (let i = 0; i < products.length; i++) {
+    value.push(products[i].price * products[i].quantity);
+  }
+  let totalPrice = value[0] + value[1] + value[2];
+
+  if (promotionCode == "SALE20") {
+    promotionCode = 0.2;
+  } else if (promotionCode == "SALE50") {
+    promotionCode = 0.5;
+  } else {
+    promotionCode = 0;
+  }
+
+  let netPrice = totalPrice * (1 - promotionCode);
+  return netPrice;
+}
+
+console.log(calculateTotalPrice(products, promotionCode));
