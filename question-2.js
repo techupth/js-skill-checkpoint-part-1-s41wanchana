5a00c931cff1a11953c79935fbbc52ed9ed8bb4a
Wanchana <s.wanchana41@gmail.com>
2024-05-22 13:46:00 +0700

    feat: 1)reassign Apple quantity, 2) assign new product, price and quantity, 3) calculate net inventory value, q.2

--- question-2.js
+++ question-2.js
@@ -4,3 +4,19 @@ const inventory = [
   { name: "Banana", price: 10, quantity: 50 },
 ];
 // เริ่มเขียนโค้ดตรงนี้
+
+//1) ให้แก้ไขจำนวนสินค้า “Apple” จาก `100` เป็น `200`
+
+inventory[0].quantity = 200;
+
+//2) เพิ่มสินค้าใหม่ที่ชื่อ “Orange” ที่มีราคา `20` บาท และมีจำนวน `300` ชิ้นในสต็อก
+
+inventory[2] = { name: "Orange", price: 20, quantity: 300 };
+
+//3) ให้คำนวณมูลค่ารวมของจำนวนสินค้าทั้งหมดในสต็อก
+
+let netValue =
+  inventory[0].price * inventory[0].quantity +
+  inventory[1].price * inventory[1].quantity +
+  inventory[2].price * inventory[2].quantity;
+console.log(netValue);
