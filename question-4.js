5259bda9da460b69cacf853f567a1a05293df1e5
Wanchana <s.wanchana41@gmail.com>
2024-05-22 14:12:14 +0700

    feat: add findLowestQuantities function that return name of the product as well, q.4

--- question-4.js
+++ question-4.js
@@ -4,4 +4,18 @@ const inventory = [
   { name: "Banana", price: 10, quantity: 50 },
   { name: "Orange", price: 30, quantity: 60 },
 ];
-// เริ่มเขียนโค้ดตรงนี้
+// เริ่มเขียนโค้ดตรงนี้ (ให้เขียนโปรแกรมในการหาสินค้าที่มีจำนวนต่ำที่สุดในคลังสินค้า)
+
+function findLowestQuantities(list) {
+  let lowestValue = Infinity;
+  let nameOfProduct = "";
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].quantity < lowestValue) {
+      lowestValue = list[i].quantity;
+      nameOfProduct = list[i].name;
+    }
+  }
+  return `สินค้าที่มีจำนวนต่ำที่สุดในคลังสินค้าคือ ${nameOfProduct} ซึ่งมี ${lowestValue} ชิ้น`;
+}
+
+console.log(findLowestQuantities(inventory));
